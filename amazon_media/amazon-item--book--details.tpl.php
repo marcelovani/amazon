8dd0e1d22b691f11c2bb92e10a7e7d22183f0af5
Randy Fay <rfay@30906.no-reply.drupal.org>
2010-09-11 15:03:43 +0000

    #761832 by rfay: Followup to Fix theming in general in D7 amazon. Sorry, I had messed up a number of tpl.php files and didn't catch it.

--- amazon_media/amazon-item--book--details.tpl.php
+++ amazon_media/amazon-item--book--details.tpl.php
@@ -1,9 +1,11 @@
+<?php
 /**
  * @file
  * Theme the 'amazon-item' 'book' 'details' style.
  * Many, many available variables. See template_preprocess_amazon_item().
  * Everything that gets put into $variables there is available.
  */
+?>
 <div class="<?php print $classes; ?>">
 <?php if (!empty($smallimage)) { print $smallimage; } ?>
 <div><strong><?php print l($title, $detailpageurl, array('html' => TRUE, 'attributes' => array('rel' => 'nofollow'))); ?></strong></div>
