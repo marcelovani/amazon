17c6a9e66bc62a933392e29aca2d98564415cdab
Randy Fay <rfay@30906.no-reply.drupal.org>
2011-01-29 23:18:43 +0000

    #1037168 by rfay: Amazon Media module needed to handle operating system value with more care

--- amazon_media/amazon-item--software--details.tpl.php
+++ amazon_media/amazon-item--software--details.tpl.php
@@ -10,6 +10,10 @@
 <?php if (!empty($smallimage)) { print $smallimage; } ?>
 <div><strong><?php print l($title, $detailpageurl, array('html' => TRUE, 'attributes' => array('rel' => 'nofollow'))); ?></strong></div>
 <div><strong><?php print t('Publisher'); ?>:</strong> <?php print $publisher; ?></div>
+<?php if (!empty($operatingsystem)):?>
 <div><strong><?php print t('Operating System'); ?>:</strong> <?php print $operatingsystem; ?></div>
-<div><strong><?php print t('Price'); ?>:</strong> <?php print $listpriceformattedprice; ?></div>
+<?php endif; ?>
+<?php if (!empty($listpriceformattedprice)): ?>
+<div><strong><?php print t('List Price'); ?>:</strong> <?php print $listpriceformattedprice; ?></div>
+<?php endif; ?>
 </div>
