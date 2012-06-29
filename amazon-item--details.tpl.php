f11d233f9d48252a4d3426c8e54aebc9b06959fe
Randy Fay <randy@randyfay.com>
2012-06-28 20:49:37 -0700

    Issue #1612118 by rfay (again)m: Fixed error when Amazon product image is missing.

--- amazon-item--details.tpl.php
+++ amazon-item--details.tpl.php
@@ -9,7 +9,7 @@
 ?>
 <div class="<?php print $classes; ?>">
 <?php if (!empty($invalid_asin)) { print "<div class='invalid_asin'>This item is no longer valid on Amazon.</div>"; } ?>
-<?php if ($smallimage): ?>
+<?php if (!empty($smallimage)): ?>
   <?php print $smallimage; ?>
 <?php endif; ?>
 <div><strong><?php print l($title, $detailpageurl, array('html' => TRUE, 'attributes' => array('rel' => 'nofollow'))); ?></strong></div>
