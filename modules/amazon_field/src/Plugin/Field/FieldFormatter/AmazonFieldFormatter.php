eef6a3dd17b9f47a5e33cc6c8d603b42f243b9f3
Mike Keran <info@mikekeran.com>
2016-07-17 12:17:32 -0700

    Partially working, still has issues saving data...

--- modules/amazon_field/src/Plugin/Field/FieldFormatter/AmazonFieldFormatter.php
+++ modules/amazon_field/src/Plugin/Field/FieldFormatter/AmazonFieldFormatter.php
@@ -7,6 +7,8 @@ use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\amazon\Amazon;
+use Drupal\Core\Url;
 
 /**
  * Plugin implementation of the 'amazon_field_formatter' formatter.
@@ -15,28 +17,69 @@ use Drupal\Core\Form\FormStateInterface;
  *   id = "amazon_field_formatter",
  *   label = @Translation("Amazon field formatter"),
  *   field_types = {
- *     "amazon_field"
+ *     "amazon_asin_field"
  *   }
  * )
  */
 class AmazonFieldFormatter extends FormatterBase {
 
   /**
+   * Contians a list of display options for this formatter.
+   *
+   * @var array
+   */
+  protected $templateOptions = [];
+
+  public function __construct($plugin_id, $plugin_definition, \Drupal\Core\Field\FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+
+    $this->templateOptions = [
+      'amazon_inline' => $this->t('Item title'),
+      'amazon_image_small' => $this->t('Small image'),
+      'amazon_image_medium' => $this->t('Medium image'),
+      'amazon_image_large' => $this->t('Large image'),
+    ];
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function defaultSettings() {
+    $defaultMaxAge = \Drupal::config('amazon.settings')->get('default_max_age');
+    if (is_null($defaultMaxAge)) {
+      throw new \InvalidArgumentException('Missing Amazon settings: default max age.');
+    }
+
     return array(
-      // Implement default settings.
-    ) + parent::defaultSettings();
+      'max_age' => $defaultMaxAge,
+      'template' => 'amazon_image_large',
+  ) + parent::defaultSettings();
   }
 
   /**
    * {@inheritdoc}
    */
   public function settingsForm(array $form, FormStateInterface $form_state) {
-    return array(
-      // Implement settings form.
-    ) + parent::settingsForm($form, $form_state);
+    $defaultMaxAge = \Drupal::config('amazon.settings')->get('default_max_age');
+
+    $form['max_age'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Max age for cached results'),
+      '#description' => $this->t('The number of seconds that the system should cache the results from Amazon\'s servers. Leave blank to use the default max age set on the <a href=":url">Amazon settings page</a>, currently set at @default_max_age seconds.', [
+        ':url' => Url::fromRoute('amazon.settings_form'),
+        '@default_max_age' => $defaultMaxAge
+      ]),
+      '#default_value' => ($this->getSetting('max_age') == $defaultMaxAge) ? '' : $this->getSetting('max_age'),
+    ];
+    $form['template'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Display item as'),
+      '#description' => $this->t('By default, all options will link to the item in the Amazon store tagged with your Associates ID.'),
+      '#options' => $this->templateOptions,
+      '#default_value' => $this->getSetting('template'),
+    ];
+
+    return $form + parent::settingsForm($form, $form_state);
   }
 
   /**
@@ -44,8 +87,8 @@ class AmazonFieldFormatter extends FormatterBase {
    */
   public function settingsSummary() {
     $summary = [];
-    // Implement settings summary.
-
+    $summary[] = $this->t('Cache max age: @max_age', ['@max_age' => $this->getSetting('max_age')]);
+    $summary[] = $this->t('Display as: @template', ['@template' => $this->templateOptions[$this->getSetting('template')]]);
     return $summary;
   }
 
@@ -55,6 +98,7 @@ class AmazonFieldFormatter extends FormatterBase {
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
 
+    dsm($items);
     foreach ($items as $delta => $item) {
       $elements[$delta] = ['#markup' => $this->viewValue($item)];
     }
@@ -72,6 +116,54 @@ class AmazonFieldFormatter extends FormatterBase {
    *   The textual output generated.
    */
   protected function viewValue(FieldItemInterface $item) {
+    dsm($item);
+    //$associatesId = \Drupal::config('amazon.settings')->get('associates_id');
+    //$amazon = new Amazon($associatesId);
+    //$results = $amazon->lookup($asin);
+    //if (empty($results[0])) {
+    //  continue;
+    //}
+    //
+    //// Build a render array for this element. This allows us to easily
+    //// override the layout by simply overriding the Twig template. It also
+    //// lets us set custom caching for each filter link.
+    //$build = [
+    //  '#results' => $results,
+    //  '#max_age' => $maxAge,
+    //];
+    //
+    //// Use the correct Twig template based on the "type" specified.
+    //switch (strtolower($type)) {
+    //  case 'inline':
+    //    $build['#theme'] = 'amazon_inline';
+    //    break;
+    //
+    //  case 'small':
+    //  case 'thumbnail':
+    //    $build['#theme'] = 'amazon_image';
+    //    $build['#size'] = 'small';
+    //    break;
+    //
+    //  case 'medium':
+    //    $build['#theme'] = 'amazon_image';
+    //    $build['#size'] = 'medium';
+    //    break;
+    //
+    //  case 'large':
+    //  case 'full':
+    //    $build['#theme'] = 'amazon_image';
+    //    $build['#size'] = 'large';
+    //    break;
+    //
+    //  default:
+    //    continue;
+    //}
+
+
+
+
+
+
     // The text value has no text format assigned to it, so the user input
     // should equal the output, including newlines.
     return nl2br(Html::escape($item->value));
--- modules/amazon_field/src/Plugin/Field/FieldType/AmazonAsinField.php
+++ modules/amazon_field/src/Plugin/Field/FieldType/AmazonAsinField.php
@@ -1,34 +1,33 @@
 <?php
 
-namespace Drupal\amazon\Plugin\Field\FieldType;
+namespace Drupal\amazon_field\Plugin\Field\FieldType;
 
 use Drupal\Component\Utility\Random;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemBase;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\Core\TypedData\DataDefinition;
 
 /**
- * Plugin implementation of the 'amazon_field' field type.
+ * Plugin implementation of the 'amazon_asin_field' field type.
  *
  * @FieldType(
- *   id = "amazon_field",
- *   label = @Translation("Amazon field"),
- *   description = @Translation("Allows display of an Amazon product based on the ASIN"),
- *   default_widget = "text_textfield",
+ *   id = "amazon_asin_field",
+ *   label = @Translation("Amazon ASIN field"),
+ *   description = @Translation("Displays an Amazon product based on the"),
+ *   default_widget = "amazon_field_widget",
  *   default_formatter = "amazon_field_formatter"
  * )
  */
-class AmazonField extends FieldItemBase {
+class AmazonAsinField extends FieldItemBase {
 
   /**
    * {@inheritdoc}
    */
   public static function defaultStorageSettings() {
     return array(
-      'max_length' => 16,
+      'max_length' => 10,
       'is_ascii' => TRUE,
       'case_sensitive' => FALSE,
     ) + parent::defaultStorageSettings();
@@ -39,8 +38,8 @@ class AmazonField extends FieldItemBase {
    */
   public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
     // Prevent early t() calls by using the TranslatableMarkup.
-    $properties['value'] = DataDefinition::create('string')
-      ->setLabel(new TranslatableMarkup('Text value'))
+    $properties['asin'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Amazon ASIN'))
       ->setSetting('case_sensitive', $field_definition->getSetting('case_sensitive'))
       ->setRequired(TRUE);
 
@@ -53,7 +52,7 @@ class AmazonField extends FieldItemBase {
   public static function schema(FieldStorageDefinitionInterface $field_definition) {
     $schema = array(
       'columns' => array(
-        'value' => array(
+        'asin' => array(
           'type' => $field_definition->getSetting('is_ascii') === TRUE ? 'varchar_ascii' : 'varchar',
           'length' => (int) $field_definition->getSetting('max_length'),
           'binary' => $field_definition->getSetting('case_sensitive'),
@@ -73,7 +72,7 @@ class AmazonField extends FieldItemBase {
     if ($max_length = $this->getSetting('max_length')) {
       $constraint_manager = \Drupal::typedDataManager()->getValidationConstraintManager();
       $constraints[] = $constraint_manager->create('ComplexData', array(
-        'value' => array(
+        'asin' => array(
           'Length' => array(
             'max' => $max_length,
             'maxMessage' => t('%name: may not be longer than @max characters.', array(
@@ -93,34 +92,15 @@ class AmazonField extends FieldItemBase {
    */
   public static function generateSampleValue(FieldDefinitionInterface $field_definition) {
     $random = new Random();
-    $values['value'] = $random->word(mt_rand(1, $field_definition->getSetting('max_length')));
+    $values['asin'] = $random->word(mt_rand(1, $field_definition->getSetting('max_length')));
     return $values;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function storageSettingsForm(array &$form, FormStateInterface $form_state, $has_data) {
-    $elements = [];
-
-    $elements['max_length'] = array(
-      '#type' => 'number',
-      '#title' => t('Maximum length'),
-      '#default_value' => $this->getSetting('max_length'),
-      '#required' => TRUE,
-      '#description' => t('The maximum length of the field in characters.'),
-      '#min' => 1,
-      '#disabled' => $has_data,
-    );
-
-    return $elements;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function isEmpty() {
-    $value = $this->get('value')->getValue();
+    $value = $this->get('asin')->getValue();
     return $value === NULL || $value === '';
   }
 
